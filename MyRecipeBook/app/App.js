ad7781265af0ae1cdb1e56e8f0a0f75723ed2c7e
Gautam Krishnan Chittaranjana <cgkrshnn@gmail.com>
2020-01-15 19:17:25 +0800

    test mongoDB stitch

--- MyRecipeBook/app/App.js
+++ MyRecipeBook/app/App.js
@@ -1,106 +1,132 @@
 import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  Stitch,
+  AnonymousCredential,
+  RemoteMongoClient,
+} from 'mongodb-stitch-react-native-sdk';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUserId: undefined,
+      client: undefined,
+    };
+    this._loadClient = this._loadClient.bind(this);
+    this._onPressLogin = this._onPressLogin.bind(this);
+    this._onPressLogout = this._onPressLogout.bind(this);
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  componentDidMount() {
+    this._loadClient();
+  }
+
+  render() {
+    let loginStatus = 'Currently logged out.';
+
+    if (this.state.currentUserId) {
+      loginStatus = `Currently logged in as ${this.state.currentUserId}!`;
+    }
+    console.log(this.state.currentUserId);
+    return (
+      <View style={styles.container}>
+        <Text> {loginStatus} </Text>
+        {this.state.currentUserId !== undefined ? (
+          <Button onPress={this._onPressLogout} title="Logout" />
+        ) : (
+          <Button onPress={this._onPressLogin} title="Login" />
+        )}
+        <Button title="Write data" onPress={this.writeData} />
+        <Button title="Read data" onPress={this.readData} />
+      </View>
+    );
+  }
+
+  _loadClient() {
+    Stitch.initializeDefaultAppClient('myrecipebook-mobua').then(client => {
+      this.setState({client});
+
+      if (client.auth.isLoggedIn) {
+        this.setState({currentUserId: client.auth.user.id});
+      }
+    });
+  }
+
+  _onPressLogin() {
+    this.state.client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(user => {
+        console.log(`Successfully logged in as user ${user.id}`);
+        this.setState({currentUserId: user.id});
+      })
+      .catch(err => {
+        console.log(`Failed to log in anonymously: ${err}`);
+        this.setState({currentUserId: undefined});
+      });
+  }
+
+  _onPressLogout() {
+    this.state.client.auth
+      .logout()
+      .then(user => {
+        console.log('Successfully logged out');
+        this.setState({currentUserId: undefined});
+      })
+      .catch(err => {
+        console.log(`Failed to log out: ${err}`);
+        this.setState({currentUserId: undefined});
+      });
+  }
+
+  writeData() {
+    const stitchAppClient = Stitch.defaultAppClient;
+    const mongoClient = stitchAppClient.getServiceClient(
+      RemoteMongoClient.factory,
+      'mongodb-myrecipebook',
+    );
+    const db = mongoClient.db('testdb');
+    const recipes = db.collection('recipes');
+    recipes
+      .insertOne({
+        name: 'Sweet Pongal',
+        cusine: 'South Indian',
+      })
+      .then(() => {
+        console.log('Wrote success');
+      })
+      .catch(err => {
+        console.warn(err);
+      });
+  }
+
+  readData() {
+    const stitchAppClient = Stitch.defaultAppClient;
+    const mongoClient = stitchAppClient.getServiceClient(
+      RemoteMongoClient.factory,
+      'mongodb-myrecipebook',
+    );
+    const db = mongoClient.db('testdb');
+    const myrecipes = db.collection('myrecipes');
+    myrecipes
+      .find({}, {})
+      .asArray()
+      .then(docs => {
+        console.log(docs[0]);
+        const ingredients = JSON.parse(docs[0].instructions);
+        console.log(ingredients);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
